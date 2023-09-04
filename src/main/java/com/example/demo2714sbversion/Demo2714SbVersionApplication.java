711e470e7d5c2d3dec800083aa3a959ed8294936
SHAKTI_GOYAL <shakti_goyal@dell.com>
2023-09-04 11:50:54 +0530

    first commit

---
+++ src/main/java/com/example/demo2714sbversion/Demo2714SbVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2714sbversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2714SbVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2714SbVersionApplication.class, args);
+	}
+
+}
